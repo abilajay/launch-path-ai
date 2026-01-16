13ac8ba36a57c26bff75d3f4c2d30f994296467c
Abil Ajay <82889749+abilajay@users.noreply.github.com>
2026-01-16 12:46:05 +0530

    Initial Commit

---
+++ launch-path-ai-service/src/main/java/com/launchpathai/launchpathaiservice/LaunchPathAiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.launchpathai.launchpathaiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchPathAiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaunchPathAiServiceApplication.class, args);
+    }
+
+}
---
+++ launch-path-ai-service/src/test/java/com/launchpathai/launchpathaiservice/LaunchPathAiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchpathai.launchpathaiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunchPathAiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
